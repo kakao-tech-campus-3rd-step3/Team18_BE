d0a8a0a98ab179af35d4a618fd9cee58cf32e1e0
jnujh <jnu21jihoon@gmail.com>
2025-08-30 21:28:58 +0900

    chore: 프로젝트 초기 설정

---
+++ src/test/java/com/kakaotech/team18/backend_server/Team18BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakaotech.team18.backend_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team18BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
