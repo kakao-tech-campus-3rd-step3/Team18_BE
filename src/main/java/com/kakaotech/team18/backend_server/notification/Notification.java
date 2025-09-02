21ff5c02ec7fdcb45baeb64bb185cef8e93f7796
ji-mim <194115@jnu.ac.kr>
2025-09-02 17:13:36 +0900

    feat: Notification 엔티티 추가 (#10)
    - 공지사항이 우리 플랫폼에서 어떻게 쓰일지 아직 논의가 부족한 것 같음

---
+++ src/main/java/com/kakaotech/team18/backend_server/notification/Notification.java
@@ -0,0 +1,31 @@
+package com.kakaotech.team18.backend_server.notification;
+
+import com.kakaotech.team18.backend_server.club.Club;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notification_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "club_id")
+    private Club club;
+
+    private String title;
+
+    private String content;
+}
