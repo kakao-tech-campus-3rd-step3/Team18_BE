53baffbc404185fdf1e80afc4c5d613b289de1e6
ji-mim <194115@jnu.ac.kr>
2025-09-02 17:11:01 +0900

    feat: ApplicationFormAnswer 엔티티 추가 (#10)
    - application에 대한 FK 값을 가지고 있어야 하는지 잘 모르겠음.
    - ApplicationForm이랑 1:1 연관 관계라고 생각했는데 어떻게 생각하시는지 궁금합니다.

---
+++ src/main/java/com/kakaotech/team18/backend_server/applicationFormAnswer/ApplicationFormAnswer.java
@@ -0,0 +1,34 @@
+package com.kakaotech.team18.backend_server.applicationFormAnswer;
+
+import com.kakaotech.team18.backend_server.application.Application;
+import com.kakaotech.team18.backend_server.applicationForm.ApplicationForm;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ApplicationFormAnswer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "application_form_answer_id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "application_id")
+    private Application application;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "application_form_id")
+    private ApplicationForm applicationForm;
+
+    private String answer;
+}
