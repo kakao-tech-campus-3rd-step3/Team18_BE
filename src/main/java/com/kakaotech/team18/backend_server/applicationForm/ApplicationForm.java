ceaff88f115776171a3ad51c778b0bcb426df620
ji-mim <194115@jnu.ac.kr>
2025-09-02 16:47:13 +0900

    feat: ApplicationForm 엔티티 추가 (#10)
    - ApplicationForm이 Application에 대해 알고 있어야 할 것 같아서 N:1 FK로 넣어뒀는데 필요한지 논의 필요

---
+++ src/main/java/com/kakaotech/team18/backend_server/applicationForm/ApplicationForm.java
@@ -0,0 +1,36 @@
+package com.kakaotech.team18.backend_server.applicationForm;
+
+import com.kakaotech.team18.backend_server.application.Application;
+import com.kakaotech.team18.backend_server.club.Club;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ApplicationForm {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "application_form_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "club_id")
+    private Club club;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "application_id")
+    private Application application;
+
+    private String title;
+
+    private String description;
+}
