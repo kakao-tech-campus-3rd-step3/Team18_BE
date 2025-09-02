aa26679e70a52d50e59a59f56fdd8740a4695b84
ji-mim <194115@jnu.ac.kr>
2025-09-02 16:59:48 +0900

    feat: Application 엔티티 추가 (#10)
    - Status enum으로 추가

---
+++ src/main/java/com/kakaotech/team18/backend_server/application/Application.java
@@ -0,0 +1,39 @@
+package com.kakaotech.team18.backend_server.application;
+
+import com.kakaotech.team18.backend_server.club.Club;
+import com.kakaotech.team18.backend_server.user.Users;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Application {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "application_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Users users;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "club_id")
+    private Club club;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+
+}
---
+++ src/main/java/com/kakaotech/team18/backend_server/application/Status.java
@@ -0,0 +1,7 @@
+package com.kakaotech.team18.backend_server.application;
+
+public enum Status {
+    PENDING,
+    PASS,
+    FAIL,
+}
