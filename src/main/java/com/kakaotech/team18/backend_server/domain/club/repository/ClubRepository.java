64d1c0d5f4d0fb776eb6db524aab549855e4f1b6
gary5876 <gjw0622@gmail.com>
2025-09-04 21:08:06 +0900

    feat : 카테고리 검색 club-repository에 추가 (kakao-tech-campus-3rd-step3#5)

--- src/main/java/com/kakaotech/team18/backend_server/domain/club/repository/ClubRepository.java
+++ src/main/java/com/kakaotech/team18/backend_server/domain/club/repository/ClubRepository.java
@@ -1,10 +1,13 @@
 package com.kakaotech.team18.backend_server.domain.club.repository;
 
+import com.kakaotech.team18.backend_server.domain.club.entity.Category;
 import com.kakaotech.team18.backend_server.domain.club.entity.Club;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClubRepository extends JpaRepository<Club, Long> {
 
+    List<Club> findByCategory(Category category);
 }
