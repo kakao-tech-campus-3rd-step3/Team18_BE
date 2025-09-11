a810b80de5ef9456f6245c5d854564d099377281
gary5876 <gjw0622@gmail.com>
2025-09-11 21:43:08 +0900

    feat(service) : serviceimpl 추가(#27)

--- src/main/java/com/kakaotech/team18/backend_server/domain/applicationForm/service/ApplicationFormServiceImpl.java
+++ src/main/java/com/kakaotech/team18/backend_server/domain/applicationForm/service/ApplicationFormServiceImpl.java
@@ -1,7 +1,19 @@
 package com.kakaotech.team18.backend_server.domain.applicationForm.service;
 
+import com.kakaotech.team18.backend_server.domain.applicationForm.repository.ApplicationFormRepository;
+import com.kakaotech.team18.backend_server.domain.applicationFormField.repository.ApplicationFormFieldRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ApplicationFormServiceImpl implements ApplicationFormService {
+    private final ApplicationFormFieldRepository applicationFormFieldRepository;
+    private final ApplicationFormRepository applicationFormRepository;
+
+    public ApplicationFormServiceImpl(
+            ApplicationFormFieldRepository applicationFormFieldRepository,
+            ApplicationFormRepository applicationFormRepository
+    ){
+        this.applicationFormFieldRepository = applicationFormFieldRepository;
+        this.applicationFormRepository = applicationFormRepository;
+    }
 }
