7e4ba2cbd3955d94cae2ee180d6600bbc2a64c02
gary5876 <gjw0622@gmail.com>
2025-09-12 00:13:08 +0900

    fix(applicationForm) : controller (#27)

---
+++ src/main/java/com/kakaotech/team18/backend_server/domain/applicationForm/controller/ApplicationFormController.java
@@ -0,0 +1,32 @@
+package com.kakaotech.team18.backend_server.domain.applicationForm.controller;
+
+import com.kakaotech.team18.backend_server.domain.applicationForm.dto.ApplicationFormResponse;
+import com.kakaotech.team18.backend_server.domain.applicationForm.service.ApplicationFormService;
+import com.kakaotech.team18.backend_server.domain.applicationFormField.dto.ApplicationFormFieldResponseDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/clubs/{clubId}")
+public class ApplicationFormController {
+
+    private ApplicationFormService applicationFormService;
+
+    @Autowired
+    public void setApplicationFormService(ApplicationFormService applicationFormService) {
+        this.applicationFormService = applicationFormService;
+    }
+
+    @GetMapping("/apply")
+    public ResponseEntity<ApplicationFormResponse> getClubById(
+            @PathVariable("clubId") Long clubId
+    ) {
+        ApplicationFormResponse response = applicationFormService.getQuestionForm(clubId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
