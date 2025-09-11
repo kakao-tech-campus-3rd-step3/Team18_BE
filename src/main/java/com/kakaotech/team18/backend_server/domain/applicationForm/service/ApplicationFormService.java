a7e65972850eae1a1cbfd14fc0e5458cb7562b5f
gary5876 <gjw0622@gmail.com>
2025-09-12 00:05:54 +0900

    fix(applicationForm) : 인터페이스에 추가 (#27)

--- src/main/java/com/kakaotech/team18/backend_server/domain/applicationForm/service/ApplicationFormService.java
+++ src/main/java/com/kakaotech/team18/backend_server/domain/applicationForm/service/ApplicationFormService.java
@@ -1,4 +1,7 @@
 package com.kakaotech.team18.backend_server.domain.applicationForm.service;
 
+import com.kakaotech.team18.backend_server.domain.applicationForm.dto.ApplicationFormResponse;
+
 public interface ApplicationFormService {
+    ApplicationFormResponse getQuestionForm(Long clubId);
 }
