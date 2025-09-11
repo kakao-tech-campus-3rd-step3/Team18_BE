57e2f568bf3c5ce22cdd221f79b064fb2c92943b
gary5876 <gjw0622@gmail.com>
2025-09-12 00:55:24 +0900

    fix(applicationForm) : controller (#27)

--- src/main/java/com/kakaotech/team18/backend_server/domain/applicationForm/controller/ApplicationFormController.java
+++ src/main/java/com/kakaotech/team18/backend_server/domain/applicationForm/controller/ApplicationFormController.java
@@ -3,7 +3,6 @@ package com.kakaotech.team18.backend_server.domain.applicationForm.controller;
 import com.kakaotech.team18.backend_server.domain.applicationForm.dto.ApplicationFormResponse;
 import com.kakaotech.team18.backend_server.domain.applicationForm.service.ApplicationFormService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
