d61bb7aecdd6b1fb87bbafb6bcc7f90aa6bc549c
jnujh <jnu21jihoon@gmail.com>
2025-09-11 14:09:42 +0900

    refactor: 수정사항에 맞게 테스트코드 변경 (#24)

--- src/test/java/com/kakaotech/team18/backend_server/global/exception/handler/GlobalExceptionHandlerTest.java
+++ src/test/java/com/kakaotech/team18/backend_server/global/exception/handler/GlobalExceptionHandlerTest.java
@@ -98,7 +98,7 @@ class GlobalExceptionHandlerTest {
 
         // then
         resultActions.andExpect(status().is(errorCode.getHttpStatus().value()))
-                .andExpect(jsonPath("$.errorCode").value(errorCode.getCode()))
+                .andExpect(jsonPath("$.error_code").value(errorCode.name()))
                 .andExpect(jsonPath("$.message").value(errorCode.getMessage()))
                 .andExpect(jsonPath("$.detail").doesNotExist()); // detail이 null 인지 확인
     }
@@ -117,7 +117,7 @@ class GlobalExceptionHandlerTest {
 
         // then
         resultActions.andExpect(status().is(errorCode.getHttpStatus().value()))
-                .andExpect(jsonPath("$.errorCode").value(errorCode.getCode()))
+                .andExpect(jsonPath("$.error_code").value(errorCode.name()))
                 .andExpect(jsonPath("$.message").value(errorCode.getMessage()))
                 .andExpect(jsonPath("$.detail").value(detail));
     }
@@ -138,7 +138,7 @@ class GlobalExceptionHandlerTest {
 
         // then
         resultActions.andExpect(status().is(errorCode.getHttpStatus().value()))
-                .andExpect(jsonPath("$.errorCode").value(errorCode.getCode()))
+                .andExpect(jsonPath("$.error_code").value(errorCode.name()))
                 .andExpect(jsonPath("$.message").value(errorCode.getMessage()))
                 .andExpect(jsonPath("$.detail").value("name: 이름은 비워둘 수 없습니다."));
     }
@@ -156,7 +156,7 @@ class GlobalExceptionHandlerTest {
 
         // then
         resultActions.andExpect(status().is(errorCode.getHttpStatus().value()))
-                .andExpect(jsonPath("$.errorCode").value(errorCode.getCode()))
+                .andExpect(jsonPath("$.error_code").value(errorCode.name()))
                 .andExpect(jsonPath("$.message").value(errorCode.getMessage()));
     }
 }
