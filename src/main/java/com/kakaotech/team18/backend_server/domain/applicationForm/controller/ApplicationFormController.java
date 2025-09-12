309b6386a0a7a67c964641ab796bf8ebbf22a270
gary5876 <gjw0622@gmail.com>
2025-09-13 00:21:08 +0900

    fix : ApplicationFormController에 @AllArgsConstructor 제거, @RequiredArgsConstructor 추가 (#35)

--- src/main/java/com/kakaotech/team18/backend_server/domain/applicationForm/controller/ApplicationFormController.java
+++ src/main/java/com/kakaotech/team18/backend_server/domain/applicationForm/controller/ApplicationFormController.java
@@ -2,15 +2,14 @@ package com.kakaotech.team18.backend_server.domain.applicationForm.controller;
 
 import com.kakaotech.team18.backend_server.domain.applicationForm.dto.ApplicationFormResponseDto;
 import com.kakaotech.team18.backend_server.domain.applicationForm.service.ApplicationFormService;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/clubs/{clubId}")
 public class ApplicationFormController {
