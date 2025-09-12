7abea2fb989768544976e367e5c23096c63c22f6
gary5876 <gjw0622@gmail.com>
2025-09-13 00:27:00 +0900

    fix : ApplicationFormFieldServiceImpl @RequiredArgsConstructor추가 (#35)

--- src/main/java/com/kakaotech/team18/backend_server/domain/applicationFormField/service/ApplicationFormFieldServiceImpl.java
+++ src/main/java/com/kakaotech/team18/backend_server/domain/applicationFormField/service/ApplicationFormFieldServiceImpl.java
@@ -2,20 +2,16 @@ package com.kakaotech.team18.backend_server.domain.applicationFormField.service;
 
 import com.kakaotech.team18.backend_server.domain.applicationFormField.entity.ApplicationFormField;
 import com.kakaotech.team18.backend_server.domain.applicationFormField.repository.ApplicationFormFieldRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @Service
 public class ApplicationFormFieldServiceImpl implements ApplicationFormFieldService {
         private final ApplicationFormFieldRepository applicationFormFieldRepository;
-
-        public ApplicationFormFieldServiceImpl(
-                ApplicationFormFieldRepository applicationFormFieldRepository
-        ) {
-                this.applicationFormFieldRepository = applicationFormFieldRepository;
-        }
 
         @Override
         @Transactional(readOnly = true)
