30b352ec4473a145dc9ac9d2eeb4d9cfa76ffb83
gary5876 <gjw0622@gmail.com>
2025-09-13 11:23:18 +0900

    refactor : ApplicationFormNotFoundException이 CustomException을 상속받도록 변경(#35)

--- src/main/java/com/kakaotech/team18/backend_server/global/exception/exceptions/ApplicationFormNotFoundException.java
+++ src/main/java/com/kakaotech/team18/backend_server/global/exception/exceptions/ApplicationFormNotFoundException.java
@@ -1,10 +1,9 @@
 package com.kakaotech.team18.backend_server.global.exception.exceptions;
 
-public class ApplicationFormNotFoundException extends RuntimeException {
-    public ApplicationFormNotFoundException(String message) {
-        super(message);
-    }
+import com.kakaotech.team18.backend_server.global.exception.code.ErrorCode;
+
+public class ApplicationFormNotFoundException extends CustomException {
     public ApplicationFormNotFoundException(Long clubId) {
-        super("해당 클럽의 활성화된 폼을 찾을 수 없습니다. clubId : "+clubId);
+        super(ErrorCode.FORM_NOT_FOUND,"해당 클럽의 활성화된 폼을 찾을 수 없습니다. clubId : "+clubId);
     }
 }
