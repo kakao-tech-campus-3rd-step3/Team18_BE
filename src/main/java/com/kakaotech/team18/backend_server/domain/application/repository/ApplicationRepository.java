1983612d60cf33fb73f0ee06af40ffea43a80aca
jnujh <jnu21jihoon@gmail.com>
2025-09-13 14:25:14 +0900

    bug: 클래스명 Users -> User 변경 적용 (#44)

--- src/main/java/com/kakaotech/team18/backend_server/domain/application/repository/ApplicationRepository.java
+++ src/main/java/com/kakaotech/team18/backend_server/domain/application/repository/ApplicationRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
-    Optional<Application> findByClub_IdAndUsers_Id(Long clubId, Long userId);
+    Optional<Application> findByClub_IdAndUser_Id(Long clubId, Long userId);
 }
