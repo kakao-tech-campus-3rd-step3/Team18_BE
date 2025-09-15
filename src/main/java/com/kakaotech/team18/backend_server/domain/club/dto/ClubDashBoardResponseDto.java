fabdbf114a02b335bc7aebeb30cc28cec92d3f0f
ji-mim <194115@jnu.ac.kr>
2025-09-15 23:11:11 +0900

    style : ClubDashBoardResponseDto 주석제거 (#22)

--- src/main/java/com/kakaotech/team18/backend_server/domain/club/dto/ClubDashBoardResponseDto.java
+++ src/main/java/com/kakaotech/team18/backend_server/domain/club/dto/ClubDashBoardResponseDto.java
@@ -5,13 +5,6 @@ import com.kakaotech.team18.backend_server.domain.applicant.dto.ApplicantRespons
 import com.kakaotech.team18.backend_server.domain.club.entity.Club;
 import java.util.List;
 
-/**
- * 총 지원자 수
- * 대기중인 지원서 수
- * 모집 일정 정보
- * 지원자 목록 조회 (이름, 학번, 학과, 전화번호, 이메일, 결과)
- */
-
 public record ClubDashBoardResponseDto(
         int totalApplicantCount,
         int pendingApplicationCount,
