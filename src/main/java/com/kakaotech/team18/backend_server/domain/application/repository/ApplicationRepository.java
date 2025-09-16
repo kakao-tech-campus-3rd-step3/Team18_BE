4002fb902a8f9f91dace3044ecc7c4b38a07efd5
ji-mim <194115@jnu.ac.kr>
2025-09-16 14:52:55 +0900

    refactor: findByClubIdAndStatus메서드에 fetch join 적용  (#22)

--- src/main/java/com/kakaotech/team18/backend_server/domain/application/repository/ApplicationRepository.java
+++ src/main/java/com/kakaotech/team18/backend_server/domain/application/repository/ApplicationRepository.java
@@ -6,9 +6,16 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
     Optional<Application> findByClub_IdAndUser_Id(Long clubId, Long userId);
 
+    @Query("""
+            select app
+            from Application app
+            join fetch app.club
+            where app.club.id = :clubId and app.status = :status
+            """)
     List<Application> findByClubIdAndStatus(Long clubId, Status status);
 }
