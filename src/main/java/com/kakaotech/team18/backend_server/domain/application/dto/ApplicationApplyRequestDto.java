539f3b4cf70ba8d8c2f65536623b7c2b737c3368
gary5876 <gjw0622@gmail.com>
2025-09-19 12:41:01 +0900

    feat : ApplicationApplyRequestDto 생성(#49)

---
+++ src/main/java/com/kakaotech/team18/backend_server/domain/application/dto/ApplicationApplyRequestDto.java
@@ -0,0 +1,28 @@
+package com.kakaotech.team18.backend_server.domain.application.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.List;
+
+public record ApplicationApplyRequestDto(
+
+        @NotBlank(message = "학번은 필수입니다.")
+        String studentId,
+
+        @NotBlank(message = "이메일은 필수입니다.")
+        @Email(message = "유효한 이메일 형식이 아닙니다.")
+        String email,
+
+        @NotBlank(message = "이름은 필수입니다.")
+        String name,
+
+        @NotBlank(message = "전화번호는 필수입니다.")
+        String phoneNumber,
+
+        @NotBlank(message = "학과는 필수입니다.")
+        String department,
+
+        List<String> answerList
+) {
+}
