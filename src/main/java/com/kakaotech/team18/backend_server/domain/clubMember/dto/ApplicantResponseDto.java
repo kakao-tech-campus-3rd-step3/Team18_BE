d9272adbc23681149cd7864f1ee3af0f78e1ac0d
jnujh <jnu21jihoon@gmail.com>
2025-09-21 21:17:11 +0900

    refactor: ApplicantResponseDto 에 Swagger @Schema 어노테이션 추가 (#58)
    ApplicantResponseDto 와 그 필드에 @Schema 어노테이션을 적용합니다.
    
    이를 통해 프론트엔드 개발자가 API 명세를 더 쉽게 이해하고 테스트할 수 있도록 합니다.

--- src/main/java/com/kakaotech/team18/backend_server/domain/clubMember/dto/ApplicantResponseDto.java
+++ src/main/java/com/kakaotech/team18/backend_server/domain/clubMember/dto/ApplicantResponseDto.java
@@ -2,13 +2,21 @@ package com.kakaotech.team18.backend_server.domain.clubMember.dto;
 
 import com.kakaotech.team18.backend_server.domain.application.entity.Status;
 import com.kakaotech.team18.backend_server.domain.clubMember.entity.ClubMember;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "대시보드 내 지원자 목록의 개별 지원자 정보")
 public record ApplicantResponseDto(
+        @Schema(description = "지원자 이름", example = "김지원")
         String name,
+        @Schema(description = "학번", example = "212121")
         String studentId,
+        @Schema(description = "학과", example = "컴퓨터공학과")
         String department,
+        @Schema(description = "전화번호", example = "010-1234-5678")
         String phoneNumber,
+        @Schema(description = "이메일", example = "test@test.com")
         String email,
+        @Schema(description = "지원서 상태", example = "PENDING")
         Status status
 ) {
 
