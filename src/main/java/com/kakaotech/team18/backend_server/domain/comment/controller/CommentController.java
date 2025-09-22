d9d82d74111a6b8cfce3efd2dc52c94bc2fdafa7
jnujh <jnu21jihoon@gmail.com>
2025-09-22 13:55:09 +0900

    refactor: import문 와일드카드 제거 (#58)

--- src/main/java/com/kakaotech/team18/backend_server/domain/comment/controller/CommentController.java
+++ src/main/java/com/kakaotech/team18/backend_server/domain/comment/controller/CommentController.java
@@ -12,9 +12,16 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "댓글/리뷰 API", description = "지원서에 대한 댓글 및 별점 관련 API")
 @RestController
