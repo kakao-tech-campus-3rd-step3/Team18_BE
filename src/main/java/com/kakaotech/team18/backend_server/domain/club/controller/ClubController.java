5929fe1f71c18e12a38f759c850f2da1a2b0bc9d
gary5876 <113762893+gary5876@users.noreply.github.com>
2025-09-23 20:54:58 +0900

    bug: fixed ClubController
    category -> {category}

--- src/main/java/com/kakaotech/team18/backend_server/domain/club/controller/ClubController.java
+++ src/main/java/com/kakaotech/team18/backend_server/domain/club/controller/ClubController.java
@@ -52,7 +52,7 @@ public class ClubController{
 
     @Operation(summary = "카테고리별 동아리 목록 조회", description = "특정 카테고리에 속한 동아리 목록을 조회합니다.")
     @ApiResponse(responseCode = "200", description = "조회 성공")
-    @GetMapping("/search/category")
+    @GetMapping("/search/{category}")
     public ResponseEntity<List<ClubListResponseDto>> listClubsByCategory(
             @Parameter(description = "조회할 동아리 카테고리", required = true, example = "SPORTS") @RequestParam String category
     ){
