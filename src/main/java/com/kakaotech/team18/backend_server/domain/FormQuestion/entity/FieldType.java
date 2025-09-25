41ead11e52eeb34e79510770d5da72011356dcb5
ji-mim <194115@jnu.ac.kr>
2025-09-25 16:45:21 +0900

    refactor: FieldType에 text 제거 (#74)
    - json으로 정보를 받을 때 한글로도 받게 하려고 작성했는데 enum 타입과 동일하게 데이터를 받는게 더 좋을 것 같아서 제거했습니다.

--- src/main/java/com/kakaotech/team18/backend_server/domain/FormQuestion/entity/FieldType.java
+++ src/main/java/com/kakaotech/team18/backend_server/domain/FormQuestion/entity/FieldType.java
@@ -2,12 +2,9 @@ package com.kakaotech.team18.backend_server.domain.FormQuestion.entity;
 
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 public enum FieldType {
-    TEXT("주관식"),
-    RADIO("라디오"),
-    CHECKBOX("체크박스"),
-    TIME_SLOT("타임슬롯");
-
-    private final String text;
+    TEXT,
+    RADIO,
+    CHECKBOX,
+    TIME_SLOT
 }
