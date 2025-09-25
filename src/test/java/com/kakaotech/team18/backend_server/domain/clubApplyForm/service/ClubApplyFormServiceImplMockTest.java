9386a807ba118ebc2803e5d36209703c0c251c35
ji-mim <194115@jnu.ac.kr>
2025-09-25 20:44:35 +0900

    test: 지원폼 수정 mock 단위 테스트 작성 (#74)

--- src/test/java/com/kakaotech/team18/backend_server/domain/clubApplyForm/service/ClubApplyFormServiceImplMockTest.java
+++ src/test/java/com/kakaotech/team18/backend_server/domain/clubApplyForm/service/ClubApplyFormServiceImplMockTest.java
@@ -11,6 +11,7 @@ import static org.mockito.Mockito.mock;
 
 import com.kakaotech.team18.backend_server.domain.FormQuestion.dto.FormQuestionRequestDto;
 import com.kakaotech.team18.backend_server.domain.FormQuestion.dto.FormQuestionResponseDto;
+import com.kakaotech.team18.backend_server.domain.FormQuestion.dto.FormQuestionUpdateDto;
 import com.kakaotech.team18.backend_server.domain.FormQuestion.dto.TimeSlotOptionRequestDto;
 import com.kakaotech.team18.backend_server.domain.FormQuestion.entity.FieldType;
 import com.kakaotech.team18.backend_server.domain.FormQuestion.entity.FormQuestion;
@@ -20,9 +21,11 @@ import com.kakaotech.team18.backend_server.domain.club.entity.Club;
 import com.kakaotech.team18.backend_server.domain.club.repository.ClubRepository;
 import com.kakaotech.team18.backend_server.domain.clubApplyForm.dto.ClubApplyFormRequestDto;
 import com.kakaotech.team18.backend_server.domain.clubApplyForm.dto.ClubApplyFormResponseDto;
+import com.kakaotech.team18.backend_server.domain.clubApplyForm.dto.ClubApplyFormUpdateDto;
 import com.kakaotech.team18.backend_server.domain.clubApplyForm.entity.ClubApplyForm;
 import com.kakaotech.team18.backend_server.domain.clubApplyForm.repository.ClubApplyFormRepository;
 import com.kakaotech.team18.backend_server.global.exception.exceptions.ApplicationFormNotFoundException;
+import com.kakaotech.team18.backend_server.global.exception.exceptions.ClubApplyFormNotFoundException;
 import com.kakaotech.team18.backend_server.global.exception.exceptions.ClubNotFoundException;
 import java.time.LocalDate;
 import java.util.List;
@@ -159,7 +162,91 @@ class ClubApplyFormServiceImplMockTest {
         then(formQuestionRepository).should(never()).save(any(FormQuestion.class));
     }
 
-    private static ClubApplyForm createClubApplyForm(Club findClub) {
+    @DisplayName("지원폼 수정 - 성공")
+    @Test
+    void updateClubApplyForm() {
+        //given
+        Long clubId = 1L;
+        Club club = mock(Club.class);
+        ReflectionTestUtils.setField(club, "id", 1L);
+        ClubApplyForm clubApplyForm = createClubApplyForm(club);
+        ReflectionTestUtils.setField(clubApplyForm, "id", 1L);
+        FormQuestion formQuestion1 = createFormQuestion(clubApplyForm);
+        ReflectionTestUtils.setField(formQuestion1, "id", 1L);
+        FormQuestion formQuestion2 = createFormQuestion(clubApplyForm);
+        ReflectionTestUtils.setField(formQuestion2, "id", 2L);
+
+        FormQuestion formQuestion3 = createFormQuestion(clubApplyForm);
+        ReflectionTestUtils.setField(formQuestion3, "id", 3L);
+
+        given(club.getId()).willReturn(1L);
+        given(clubRepository.findById(clubId)).willReturn(Optional.of(club));
+        given(clubApplyFormRepository.findByClubId(clubId)).willReturn(Optional.of(clubApplyForm));
+        given(formQuestionRepository.findByClubApplyForm(clubApplyForm)).willReturn(List.of(formQuestion1, formQuestion2));
+
+        // Update: question1 modified, question2 deleted, new question3 added
+        ClubApplyFormUpdateDto requestDto = new ClubApplyFormUpdateDto(
+                "수정된 지원서 제목",
+                "수정된 지원서 설명",
+                List.of(
+                        new com.kakaotech.team18.backend_server.domain.FormQuestion.dto.FormQuestionUpdateDto(
+                                1L,
+                                "수정된 질문 1",
+                                FieldType.TEXT,
+                                false,
+                                1L,
+                                null,
+                                null
+                        ),
+                        new com.kakaotech.team18.backend_server.domain.FormQuestion.dto.FormQuestionUpdateDto(
+                                null,
+                                "새로운 질문 3",
+                                FieldType.CHECKBOX,
+                                true,
+                                3L,
+                                List.of("옵션 A", "옵션 B"),
+                                null
+                        )
+                )
+        );
+
+        //when
+        clubApplyFormService.updateClubApplyForm(clubId, requestDto);
+
+        //then
+        then(clubRepository).should(times(1)).findById(clubId);
+        then(clubApplyFormRepository).should(times(1)).findByClubId(clubId);
+        then(formQuestionRepository).should(times(1)).findByClubApplyForm(clubApplyForm);
+        then(formQuestionRepository).should(times(1)).save(any(FormQuestion.class)); // For the new question
+        then(formQuestionRepository).should(times(1)).deleteById(2L); // For the deleted question
+    }
+
+    @DisplayName("지원폼 수정 - 실패 (지원폼이 존재하지 않음)")
+    @Test
+    void updateClubApplyForm_ClubApplyFormNotFound() {
+        //given
+        Long clubId = 1L;
+        Club club = mock(Club.class);
+        ReflectionTestUtils.setField(club, "id", 1L);
+        FormQuestionUpdateDto question1 = new FormQuestionUpdateDto(1L, "질문 1", FieldType.TEXT, true, 1L, null, null);
+        ClubApplyFormUpdateDto requestDto = new ClubApplyFormUpdateDto("테스트 지원서", "테스트 설명", List.of(question1));
+
+        given(club.getId()).willReturn(1L);
+        given(clubRepository.findById(clubId)).willReturn(Optional.of(club));
+        given(clubApplyFormRepository.findByClubId(clubId)).willReturn(Optional.empty());
+
+        //when, then
+        Assertions.assertThatThrownBy(() -> clubApplyFormService.updateClubApplyForm(clubId, requestDto))
+                .isInstanceOf(ClubApplyFormNotFoundException.class)
+                .hasMessageContaining("지원폼이 존재하지 않습니다");
+
+        then(clubRepository).should(times(1)).findById(clubId);
+        then(clubApplyFormRepository).should(times(1)).findByClubId(clubId);
+        then(clubApplyFormRepository).should(never()).save(any(ClubApplyForm.class));
+        then(formQuestionRepository).should(never()).save(any(FormQuestion.class));    }
+
+
+    private ClubApplyForm createClubApplyForm(Club findClub) {
         return ClubApplyForm.builder()
                 .club(findClub)
                 .title("먹짱 동아리 지원서")
@@ -167,7 +254,7 @@ class ClubApplyFormServiceImplMockTest {
                 .build();
     }
 
-    private static FormQuestion createFormQuestion(ClubApplyForm savedClubApplyForm) {
+    private FormQuestion createFormQuestion(ClubApplyForm savedClubApplyForm) {
         return FormQuestion.builder()
                 .clubApplyForm(savedClubApplyForm)
                 .question("가장 좋아하는 음식은 무엇인가요?")
@@ -180,6 +267,4 @@ class ClubApplyFormServiceImplMockTest {
                 )))
                 .build();
     }
-
-
 }
