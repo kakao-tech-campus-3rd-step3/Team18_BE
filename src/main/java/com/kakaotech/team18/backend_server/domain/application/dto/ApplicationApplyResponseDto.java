75d161570726e1243e77c48504539f209e45df79
gary5876 <gjw0622@gmail.com>
2025-09-26 15:55:50 +0900

    fix:applicationApplyResponseDto에서 @Builder제거(#49)

--- src/main/java/com/kakaotech/team18/backend_server/domain/application/dto/ApplicationApplyResponseDto.java
+++ src/main/java/com/kakaotech/team18/backend_server/domain/application/dto/ApplicationApplyResponseDto.java
@@ -1,10 +1,7 @@
 package com.kakaotech.team18.backend_server.domain.application.dto;
 
-import lombok.Builder;
-
 import java.time.LocalDateTime;
 
-@Builder
 public record ApplicationApplyResponseDto(
         String studentId,
         LocalDateTime submittedAt,
