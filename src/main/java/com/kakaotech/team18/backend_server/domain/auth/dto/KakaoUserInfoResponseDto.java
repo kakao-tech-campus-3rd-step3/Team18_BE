62ea6a0f0c8b65d5b80fef9afbce8b4665cae1f2
jnujh <jnu21jihoon@gmail.com>
2025-09-30 21:00:09 +0900

    fix: Kakao API 응답 변경에 대한 안정성 강화 (#56)
    카카오 API 응답에 새로운 필드가 추가될 경우, Jackson 라이브러리가
    UnrecognizedPropertyException을 발생시켜 로그인 흐름이 실패하는
    잠재적 위험이 있었습니다.
    
    KakaoUserInfoResponseDto와 내부 Properties 클래스에
    @JsonIgnoreProperties(ignoreUnknown = true) 어노테이션을 추가했습니다.
    
    이를 통해 DTO에 정의되지 않은 필드는 역직렬화 과정에서 안전하게
    무시되도록 하여, 외부 API 변경에 대한 시스템의 안정성을 높였습니다.

--- src/main/java/com/kakaotech/team18/backend_server/domain/auth/dto/KakaoUserInfoResponseDto.java
+++ src/main/java/com/kakaotech/team18/backend_server/domain/auth/dto/KakaoUserInfoResponseDto.java
@@ -1,11 +1,13 @@
 package com.kakaotech.team18.backend_server.domain.auth.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class KakaoUserInfoResponseDto {
 
     private Long id; // 사용자의 고유 ID (kakaoId)
@@ -15,6 +17,7 @@ public class KakaoUserInfoResponseDto {
 
     @Data
     @NoArgsConstructor
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Properties {
         private String nickname;
     }
