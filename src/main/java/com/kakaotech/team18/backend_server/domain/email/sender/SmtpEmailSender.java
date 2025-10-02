a129fda45bba4dd2688719dce50fe7c2a24433c2
gary5876 <gjw0622@gmail.com>
2025-10-02 20:59:04 +0900

    fix:SmtpEmailSender에 @RequiredArgsConstructor 추가(#80)

--- src/main/java/com/kakaotech/team18/backend_server/domain/email/sender/SmtpEmailSender.java
+++ src/main/java/com/kakaotech/team18/backend_server/domain/email/sender/SmtpEmailSender.java
@@ -2,20 +2,18 @@ package com.kakaotech.team18.backend_server.domain.email.sender;
 
 import java.util.List;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 import jakarta.mail.internet.MimeMessage;
 
+@RequiredArgsConstructor
 @Component
 public class SmtpEmailSender implements EmailSender {
 
     private final JavaMailSender mailSender;
-
-    public SmtpEmailSender(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
-    }
 
     @Override
     public void sendHtml(String from, String replyTo, List<String> to, String subject, String htmlBody) {
