57ad4d446c89f3b58faf1aadf390c64104ef41ff
jnujh <jnu21jihoon@gmail.com>
2025-10-02 18:18:37 +0900

    test: ci 해결 확인을 위한 테스트

--- src/main/java/com/kakaotech/team18/backend_server/Team18BeApplication.java
+++ src/main/java/com/kakaotech/team18/backend_server/Team18BeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Team18BeApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(Team18BeApplication.class, args);
 	}
