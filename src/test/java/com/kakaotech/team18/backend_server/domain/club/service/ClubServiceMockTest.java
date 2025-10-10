942d9d9c7dfd50a853db036fea1a8218dc863f80
ji-mim <194115@jnu.ac.kr>
2025-10-10 16:25:37 +0900

    refactor: ClubServiceMockTest 정상화 (#78)
    - 지원 유의사항 관련된 테스트를 제거하고, 테스트가 깨지지 않도록 수정했습니다.

--- src/test/java/com/kakaotech/team18/backend_server/domain/club/service/ClubServiceMockTest.java
+++ src/test/java/com/kakaotech/team18/backend_server/domain/club/service/ClubServiceMockTest.java
@@ -11,14 +11,10 @@ import static org.mockito.Mockito.verifyNoInteractions;
 import com.kakaotech.team18.backend_server.domain.application.entity.Application;
 import com.kakaotech.team18.backend_server.domain.application.entity.Status;
 import com.kakaotech.team18.backend_server.domain.application.repository.ApplicationRepository;
-import com.kakaotech.team18.backend_server.domain.club.dto.CautionItemResponseDto;
-import com.kakaotech.team18.backend_server.domain.club.dto.ClubCautionResponseDto;
 import com.kakaotech.team18.backend_server.domain.club.dto.ClubDashBoardResponseDto;
 import com.kakaotech.team18.backend_server.domain.club.dto.ClubDetailResponseDto;
 import com.kakaotech.team18.backend_server.domain.club.entity.Category;
-import com.kakaotech.team18.backend_server.domain.club.entity.CautionItem;
 import com.kakaotech.team18.backend_server.domain.club.entity.Club;
-import com.kakaotech.team18.backend_server.domain.club.entity.ClubCaution;
 import com.kakaotech.team18.backend_server.domain.club.entity.ClubImage;
 import com.kakaotech.team18.backend_server.domain.club.entity.ClubIntroduction;
 import com.kakaotech.team18.backend_server.domain.club.repository.ClubRepository;
@@ -44,8 +40,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
-import org.mockito.MockedStatic;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -168,9 +164,6 @@ public class ClubServiceMockTest {
     @Test
     void getClubDetail() {
         //given
-        CautionItem cautionItem = createCautionItem();
-        ClubCaution clubCaution = createClubCaution();
-        clubCaution.addItem(cautionItem);
         Long clubId = 1L;
         ClubIntroduction clubIntroduction = createClubIntroduction();
         ReflectionTestUtils.setField(clubIntroduction, "id", 1L);
@@ -178,7 +171,6 @@ public class ClubServiceMockTest {
         clubIntroduction.addImage(image);
         ReflectionTestUtils.setField(image, "id", 1L);
         Club club = createClub(clubIntroduction, LocalDateTime.of(2025, 9, 3, 0, 0), LocalDateTime.of(2025, 9, 20, 23, 59));
-        club.addCaution(clubCaution);
         ReflectionTestUtils.setField(club, "id", 1L);
         User user = createUser("loginId", "123456");
         ReflectionTestUtils.setField(user, "id", 1L);
@@ -210,7 +202,7 @@ public class ClubServiceMockTest {
                     "010-1234-5678",
                     LocalDateTime.of(2025, 9, 3, 0, 0),
                     LocalDateTime.of(2025, 9, 20, 23, 59),
-                    List.of(new ClubCautionResponseDto(1, "지원 자격 확인", List.of(new CautionItemResponseDto(1, "해당 학기 재학 중인 학생만 지원 가능합니다."))))
+                    "주의사항"
             );
 
             //when
@@ -222,82 +214,6 @@ public class ClubServiceMockTest {
             verify(clubMemberRepository).findClubAdminByClubIdAndRole(eq(clubId), eq(Role.CLUB_ADMIN));
         }
     }
-
-    @DisplayName("Club Detail 조회시 Caution과 CautionItem이 displayOrder에 따라 정렬되어 반환된다.")
-    @Test
-    void getClubDetailWithSortedCautionAndCautionItem() {
-        //given
-        CautionItem cautionItem1 = CautionItem.builder().displayOrder(1).content("아이템1").build();
-        CautionItem cautionItem2 = CautionItem.builder().displayOrder(2).content("아이템2").build();
-
-        ClubCaution clubCaution1 = ClubCaution.builder().displayOrder(1).title("주의사항1").build();
-        clubCaution1.addItem(cautionItem2); // 의도적으로 순서 뒤집기
-        clubCaution1.addItem(cautionItem1);
-
-        CautionItem cautionItem3 = CautionItem.builder().displayOrder(1).content("아이템3").build();
-        ClubCaution clubCaution2 = ClubCaution.builder().displayOrder(2).title("주의사항2").build();
-        clubCaution2.addItem(cautionItem3);
-
-
-        Long clubId = 1L;
-        ClubIntroduction clubIntroduction = createClubIntroduction();
-        ReflectionTestUtils.setField(clubIntroduction, "id", 1L);
-        ClubImage image = createClubImage(clubIntroduction);
-        clubIntroduction.addImage(image);
-        ReflectionTestUtils.setField(image, "id", 1L);
-        Club club = createClub(clubIntroduction, LocalDateTime.of(2025, 9, 3, 0, 0), LocalDateTime.of(2025, 9, 20, 23, 59));
-        club.addCaution(clubCaution2); // 의도적으로 순서 뒤집기
-        club.addCaution(clubCaution1);
-        ReflectionTestUtils.setField(club, "id", 1L);
-        User user = createUser("loginId", "123456");
-        ReflectionTestUtils.setField(user, "id", 1L);
-        ClubMember clubMember = createClubMember(user, club, mock(Application.class), Role.CLUB_ADMIN, ActiveStatus.ACTIVE);
-        ReflectionTestUtils.setField(clubMember, "id", 1L);
-
-        try (MockedStatic<RecruitStatusCalculator> mockedCalculator = Mockito.mockStatic(RecruitStatusCalculator.class)) {
-            // given
-            mockedCalculator.when(() -> RecruitStatusCalculator.calculate(club.getRecruitStart(), club.getRecruitEnd()))
-                    .thenReturn(RecruitStatus.RECRUITING);
-
-            given(clubRepository.findClubDetailById(eq(clubId))).willReturn(Optional.of(club));
-            given(clubMemberRepository.findClubAdminByClubIdAndRole(eq(clubId), eq(Role.CLUB_ADMIN))).willReturn(Optional.of(clubMember));
-
-            ClubDetailResponseDto expect = new ClubDetailResponseDto(
-                    "카태켐",
-                    "공7 1호관",
-                    Category.LITERATURE,
-                    "함께 배우는 카태켐",
-                    List.of("image1.url"),
-                    "overview",
-                    "activities",
-                    "ideal",
-                    "매주 화요일 오후 6시반",
-                    "모집중",
-                    "김춘식",
-                    "010-1234-5678",
-                    LocalDateTime.of(2025, 9, 3, 0, 0),
-                    LocalDateTime.of(2025, 9, 20, 23, 59),
-                    List.of(
-                            new ClubCautionResponseDto(1, "주의사항1", List.of(
-                                    new CautionItemResponseDto(1, "아이템1"),
-                                    new CautionItemResponseDto(2, "아이템2")
-                            )),
-                            new ClubCautionResponseDto(2, "주의사항2", List.of(
-                                    new CautionItemResponseDto(1, "아이템3")
-                            ))
-                    )
-            );
-
-            //when
-            ClubDetailResponseDto actual = clubService.getClubDetail(clubId);
-
-            //then
-            assertThat(actual).isEqualTo(expect);
-            verify(clubRepository).findClubDetailById(eq(clubId));
-            verify(clubMemberRepository).findClubAdminByClubIdAndRole(eq(clubId), eq(Role.CLUB_ADMIN));
-        }
-    }
-
 
     @DisplayName("Club Detail 조회시 존재하지 않는 clubId를 사용할 때 ClubNotFoundException이 실행된다.")
     @Test
@@ -403,19 +319,6 @@ public class ClubServiceMockTest {
         assertThat(actual).isEqualTo(expect);
     }
 
-    private static CautionItem createCautionItem() {
-        return CautionItem.builder()
-                .displayOrder(1)
-                .content("해당 학기 재학 중인 학생만 지원 가능합니다.")
-                .build();
-    }
-
-    private static ClubCaution createClubCaution() {
-        return ClubCaution.builder()
-                .displayOrder(1)
-                .title("지원 자격 확인")
-                .build();
-    }
 
 
     private static ClubImage createClubImage(ClubIntroduction clubIntroduction) {
@@ -444,14 +347,18 @@ public class ClubServiceMockTest {
     }
 
 
-    private static Club createClub(ClubIntroduction clubIntroduction, LocalDateTime recruitStart,
-            LocalDateTime recruitEnd) {
+    private static Club createClub(
+            ClubIntroduction clubIntroduction,
+            LocalDateTime recruitStart,
+            LocalDateTime recruitEnd
+    ) {
         return Club.builder()
                 .name("카태켐")
                 .category(Category.LITERATURE)
                 .location("공7 1호관")
                 .shortIntroduction("함께 배우는 카태켐")
                 .introduction(clubIntroduction)
+                .caution("주의사항")
                 .recruitStart(recruitStart)
                 .recruitEnd(recruitEnd)
                 .regularMeetingInfo("매주 화요일 오후 6시반")
