b80daca7322d3623ba6ec5f31f14ecb62c0112e5
ji-mim <194115@jnu.ac.kr>
2025-10-14 23:33:57 +0900

    feat: ClubReviewService interface 및 RequestDTO 생성(#138)

---
+++ src/main/java/com/kakaotech/team18/backend_server/domain/clubReview/service/ClubReviewService.java
@@ -0,0 +1,8 @@
+package com.kakaotech.team18.backend_server.domain.clubReview.service;
+
+import com.kakaotech.team18.backend_server.domain.clubReview.dto.ClubReviewRequestDto;
+import com.kakaotech.team18.backend_server.global.dto.SuccessResponseDto;
+
+public interface ClubReviewService {
+    SuccessResponseDto createClubReview(Long clubId, ClubReviewRequestDto request);
+}
