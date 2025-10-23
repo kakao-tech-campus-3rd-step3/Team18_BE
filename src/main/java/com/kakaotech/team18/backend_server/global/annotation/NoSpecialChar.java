71a58ad8015b1162e53656b8774546973ddb49e5
ji-mim <194115@jnu.ac.kr>
2025-10-23 15:46:10 +0900

    refactor: import 와일드 카드 제거 (#140)

--- src/main/java/com/kakaotech/team18/backend_server/global/annotation/NoSpecialChar.java
+++ src/main/java/com/kakaotech/team18/backend_server/global/annotation/NoSpecialChar.java
@@ -2,7 +2,11 @@ package com.kakaotech.team18.backend_server.global.annotation;
 
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Documented
 @Constraint(validatedBy = NoSpecialCharValidator.class)
