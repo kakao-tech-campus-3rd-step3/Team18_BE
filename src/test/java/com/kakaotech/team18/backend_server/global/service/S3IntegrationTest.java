74da4f98a8582940f5649d498f76ce50bb444fcf
ji-mim <194115@jnu.ac.kr>
2025-10-28 15:47:01 +0900

    test: S3 Service localStack 단위 테스트 작성 (#151)
    - aws s3에 어떻게 테스트를 작성할 수 있을까 하다가 localStack이라고 도커로 서버 띄워서 S3 서버를 띄워 요청을 보낼 수 있는게 있어서 한번 작성해봤습니다. 근데 이걸 사용하려면 테스트 환경에서 (로컬) 도커가 실행되고 있어야 하는데 CI 과정에서 이걸 적용하기가 복잡한 것 같아서 Disabled 처리해뒀습니다. 로컬에서 해봤을 때는 테스트 통과한걸 확인했습니다.

---
+++ src/test/java/com/kakaotech/team18/backend_server/global/service/S3IntegrationTest.java
@@ -0,0 +1,59 @@
+package com.kakaotech.team18.backend_server.global.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.kakaotech.team18.backend_server.domain.club.entity.ClubImage;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.mock.web.MockMultipartFile;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Testcontainers
+class S3IntegrationTest {
+
+    @Container
+    static LocalStackContainer localStack = new LocalStackContainer(DockerImageName.parse("localstack/localstack:latest"))
+            .withServices(LocalStackContainer.Service.S3)
+            .withExposedPorts(4566);
+
+    @Autowired
+    private AmazonS3 amazonS3;
+
+    @Autowired
+    private S3Service s3Service;
+
+    @Test
+    @Disabled("LocalStack 환경 필요 - CI에서는 무시")
+    void uploadAndDeleteFile() throws Exception {
+        // given
+        String bucketName = "test-bucket213";
+        amazonS3.createBucket(bucketName);
+        ReflectionTestUtils.setField(s3Service, "bucket", bucketName);
+
+        MultipartFile file = new MockMultipartFile(
+                "image", "sample.png", "image/png", "data".getBytes()
+        );
+
+        // when
+        String url = s3Service.upload(file);
+
+        // then
+        assertThat(amazonS3.doesObjectExist(bucketName, extractKey(url))).isTrue();
+
+        s3Service.deleteFile(ClubImage.builder().imageUrl(url).build());
+        assertThat(amazonS3.doesObjectExist(bucketName, extractKey(url))).isFalse();
+    }
+
+    private String extractKey(String url) {
+        return url.substring(url.lastIndexOf("/") + 1);
+    }
+}
