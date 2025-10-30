db5585bdc3712ace6118821fa4c2079e6315629d
jnujh <jnu21jihoon@gmail.com>
2025-10-30 15:20:17 +0900

    refactor: 파라미터명 통일

--- src/main/java/com/kakaotech/team18/backend_server/global/exception/exceptions/DuplicateKakaoIdException.java
+++ src/main/java/com/kakaotech/team18/backend_server/global/exception/exceptions/DuplicateKakaoIdException.java
@@ -3,7 +3,7 @@ package com.kakaotech.team18.backend_server.global.exception.exceptions;
 import com.kakaotech.team18.backend_server.global.exception.code.ErrorCode;
 
 public class DuplicateKakaoIdException extends CustomException {
-    public DuplicateKakaoIdException(String message) {
-        super(ErrorCode.DUPLICATE_KAKAO_ID, message);
+    public DuplicateKakaoIdException(String detail) {
+        super(ErrorCode.DUPLICATE_KAKAO_ID, detail);
     }
 }
