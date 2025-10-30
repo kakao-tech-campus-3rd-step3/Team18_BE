5667c6621ec66f859222f067e6847a5e7da2c10a
gary5876 <gjw0622@gmail.com>
2025-10-31 00:13:13 +0900

    fix:fixed duplicate s3 config problem(#169)

--- src/main/java/com/kakaotech/team18/backend_server/domain/s3/properties/S3Properties.java
+++
@@ -1,8 +0,0 @@
-package com.kakaotech.team18.backend_server.domain.s3.properties;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "cloud.aws.s3")
-public record S3Properties(String region, Credentials credentials) {
-    public record Credentials(String accessKey, String secretKey) {}
-}
