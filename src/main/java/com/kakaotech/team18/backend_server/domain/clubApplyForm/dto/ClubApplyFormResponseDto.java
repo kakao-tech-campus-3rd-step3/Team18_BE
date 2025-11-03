3cc7140aaf8f7e8bbbb32fd55bd7535e3cc4591d
ji-mim <194115@jnu.ac.kr>
2025-11-03 11:24:16 +0900

    refactor: ClubApplyFormResponseDto의 필드명 수정 및 recruitDate 필드 추가 (#156)
    - dto 필드명을 등록/수정과 통일시키고 recruitDate 필드를 추가했습니다. 이 값은 Club의 recruitStart와 recruitEnd 값을 파라미터로 받아서 2024-09-01 ~ 2024-09-30 이런 식으로 변환되어 들어갑니다. 이 값을 찾기 위해서 getQuestionForm 메서드에 findClub 메서드를 사용해 club을 조회합니다.

--- src/main/java/com/kakaotech/team18/backend_server/domain/clubApplyForm/dto/ClubApplyFormResponseDto.java
+++ src/main/java/com/kakaotech/team18/backend_server/domain/clubApplyForm/dto/ClubApplyFormResponseDto.java
@@ -2,6 +2,7 @@ package com.kakaotech.team18.backend_server.domain.clubApplyForm.dto;
 
 import com.kakaotech.team18.backend_server.domain.formQuestion.dto.FormQuestionResponseDto;
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Schema(description = "지원서 양식 조회 응답 데이터")
@@ -10,14 +11,19 @@ public record ClubApplyFormResponseDto(
         String title,
         @Schema(description = "지원서 양식 설명", example = "함께 성장할 열정적인 신입 부원을 모집합니다!")
         String description,
+        @Schema(description = "모집 기간", example = "2024-09-01 ~ 2024-09-30")
+        String recruitDate,
         @Schema(description = "질문 목록")
-        List<FormQuestionResponseDto> questions
+        List<FormQuestionResponseDto> formQuestions
 ) {
     public static ClubApplyFormResponseDto of(
             String title,
             String description,
+            LocalDateTime recruitStart,
+            LocalDateTime recruitEnd,
             List<FormQuestionResponseDto> questions
     ) {
-        return new ClubApplyFormResponseDto(title, description, questions);
+        String recruitDate = String.format("%s ~ %s", recruitStart.toLocalDate().toString(), recruitEnd.toLocalDate().toString());
+        return new ClubApplyFormResponseDto(title, description, recruitDate, questions);
     }
 }
