f5ebf302da27db50065eaad72add54ac25ab95bc
ji-mim <194115@jnu.ac.kr>
2025-11-03 11:53:18 +0900

    refactor: AuthController에 필드명 교체 적용 (#145)

--- src/main/java/com/kakaotech/team18/backend_server/domain/auth/controller/AuthController.java
+++ src/main/java/com/kakaotech/team18/backend_server/domain/auth/controller/AuthController.java
@@ -54,7 +54,7 @@ public class AuthController {
                     .maxAge(jwtProperties.refreshTokenValidityInSeconds())
                     .build();
 
-            LoginSuccessResponseDto.Body body = new LoginSuccessResponseDto.Body(dto.status(), dto.accessToken(), dto.clubIdAndRoleList());
+            LoginSuccessResponseDto.Body body = new LoginSuccessResponseDto.Body(dto.status(), dto.accessToken(), dto.clubAndRoleList());
 
             return ResponseEntity.ok()
                     .header(HttpHeaders.SET_COOKIE, responseCookie.toString())
@@ -85,7 +85,7 @@ public class AuthController {
                 .maxAge(jwtProperties.refreshTokenValidityInSeconds())
                 .build();
 
-        LoginSuccessResponseDto.Body body = new LoginSuccessResponseDto.Body(responseDto.status(), responseDto.accessToken(), responseDto.clubIdAndRoleList());
+        LoginSuccessResponseDto.Body body = new LoginSuccessResponseDto.Body(responseDto.status(), responseDto.accessToken(), responseDto.clubAndRoleList());
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .header(HttpHeaders.SET_COOKIE, responseCookie.toString())
