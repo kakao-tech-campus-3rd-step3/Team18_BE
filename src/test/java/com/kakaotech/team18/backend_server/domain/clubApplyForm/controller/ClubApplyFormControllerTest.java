f5ea59750ee506cf265fe544709544ad8ada396c
gary5876 <gjw0622@gmail.com>
2025-11-04 20:58:22 +0900

    test:fixed test according to the change

--- src/test/java/com/kakaotech/team18/backend_server/domain/clubApplyForm/controller/ClubApplyFormControllerTest.java
+++ src/test/java/com/kakaotech/team18/backend_server/domain/clubApplyForm/controller/ClubApplyFormControllerTest.java
@@ -17,10 +17,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kakaotech.team18.backend_server.domain.clubApplyForm.dto.ClubApplyFormRequestDto;
 import com.kakaotech.team18.backend_server.domain.clubApplyForm.dto.ClubApplyFormResponseDto;
 import com.kakaotech.team18.backend_server.domain.clubApplyForm.dto.ClubApplyFormUpdateDto;
+import com.kakaotech.team18.backend_server.domain.clubApplyForm.dto.UserClubApplyFormResponseDto;
 import com.kakaotech.team18.backend_server.domain.clubApplyForm.service.ClubApplyFormService;
 import com.kakaotech.team18.backend_server.domain.formQuestion.dto.FormQuestionRequestDto;
 import com.kakaotech.team18.backend_server.domain.formQuestion.dto.FormQuestionResponseDto;
 import com.kakaotech.team18.backend_server.domain.formQuestion.dto.FormQuestionUpdateDto;
+import com.kakaotech.team18.backend_server.domain.formQuestion.dto.UserFormQuestionResponseDto;
 import com.kakaotech.team18.backend_server.domain.formQuestion.entity.FieldType;
 import com.kakaotech.team18.backend_server.global.config.SecurityConfig;
 import com.kakaotech.team18.backend_server.global.config.TestSecurityConfig;
@@ -64,9 +66,9 @@ class ClubApplyFormControllerTest {
     void getClubApplyFormByClubId_success() throws Exception {
         // given
         Long clubId = 1L;
-        FormQuestionResponseDto question1 = new FormQuestionResponseDto(1L,1L, FieldType.TEXT, "이름", true, null, null);
-        FormQuestionResponseDto question2 = new FormQuestionResponseDto(2L, 2L, FieldType.RADIO, "성별", true, List.of("남", "여"), null);
-        ClubApplyFormResponseDto mockResponse = ClubApplyFormResponseDto.of(
+        UserFormQuestionResponseDto question1 = new UserFormQuestionResponseDto(1L,"이름", FieldType.TEXT,  true, null, 1L,null);
+        UserFormQuestionResponseDto question2 = new UserFormQuestionResponseDto(2L, "성별", FieldType.RADIO,  true, List.of("남", "여"), 2L,null);
+        UserClubApplyFormResponseDto mockResponse = UserClubApplyFormResponseDto.of(
                 "테스트 동아리 지원서",
                 "테스트 동아리 지원서 설명입니다.",
                 LocalDateTime.of(2024, 9, 1, 0, 0),
@@ -74,7 +76,7 @@ class ClubApplyFormControllerTest {
                 List.of(question1, question2)
         );
 
-        when(clubApplyFormService.getQuestionForm(clubId)).thenReturn(mockResponse);
+        when(clubApplyFormService.getUserQuestionForm(clubId)).thenReturn(mockResponse);
 
         // when & then
         mockMvc.perform(get("/api/clubs/{clubId}/apply", clubId))
@@ -87,7 +89,7 @@ class ClubApplyFormControllerTest {
                 .andExpect(jsonPath("$.recruitDate").value("2024-09-01 ~ 2024-09-30"))
                 .andExpect(jsonPath("$.formQuestions[1].optionList[0]").value("남"));
 
-        verify(clubApplyFormService, times(1)).getQuestionForm(clubId);
+        verify(clubApplyFormService, times(1)).getUserQuestionForm(clubId);
     }
     @DisplayName("대시보드 api 동아리 지원서 양식 조회 테스트 - 성공")
     @Test
@@ -125,7 +127,7 @@ class ClubApplyFormControllerTest {
     void getClubApplyFormByClubId_notFound() throws Exception {
         // given
         Long clubId = 999L;
-        when(clubApplyFormService.getQuestionForm(clubId))
+        when(clubApplyFormService.getUserQuestionForm(clubId))
                 .thenThrow(new ClubApplyFormNotFoundException("clubId = " + clubId));
 
         // when & then
@@ -133,7 +135,7 @@ class ClubApplyFormControllerTest {
                 .andDo(print())
                 .andExpect(status().isNotFound());
 
-        verify(clubApplyFormService, times(1)).getQuestionForm(clubId);
+        verify(clubApplyFormService, times(1)).getUserQuestionForm(clubId);
     }
     @DisplayName("대시보드 api 동아리 지원서 양식 조회 테스트 - 지원서 양식 없음")
     @Test
