47ecf009d33611051a1d777039cc21a75bbd8b4f
jnujh <jnu21jihoon@gmail.com>
2025-11-05 21:25:54 +0900

    fix: 요청 쿠키에 refreshtoken 없는 상황 에러 수정

--- src/main/java/com/kakaotech/team18/backend_server/global/exception/code/ErrorCode.java
+++ src/main/java/com/kakaotech/team18/backend_server/global/exception/code/ErrorCode.java
@@ -26,6 +26,7 @@ public enum ErrorCode {
 
     // 400 BAD_REQUEST: 잘못된 요청
     INVALID_INPUT_VALUE("입력 값이 올바르지 않습니다.", HttpStatus.BAD_REQUEST),
+    REQUIRED_COOKIE_NOT_FOUND("필수 쿠키가 요청에 포함되지 않았습니다.", HttpStatus.BAD_REQUEST),
     INVALID_RATING_UNIT("별점은 0.5 단위로만 입력 가능합니다.", HttpStatus.BAD_REQUEST),
     DUPLICATE_KAKAO_ID("이미 다른 계정과 연동된 학번입니다.", HttpStatus.BAD_REQUEST),
     INVALID_INPUT_ANSWER("잘못된 답안 입력입니다", HttpStatus.BAD_REQUEST),
