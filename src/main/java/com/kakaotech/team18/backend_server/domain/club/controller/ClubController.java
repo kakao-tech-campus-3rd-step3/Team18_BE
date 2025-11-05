10ea5dfb2f23e63dfd70d2679ffc763f453ece04
ji-mim <194115@jnu.ac.kr>
2025-11-05 23:00:31 +0900

    refactor: 이미지 수정 API PUT으로 변경

--- src/main/java/com/kakaotech/team18/backend_server/domain/club/controller/ClubController.java
+++ src/main/java/com/kakaotech/team18/backend_server/domain/club/controller/ClubController.java
@@ -26,6 +26,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -81,7 +82,7 @@ public class ClubController{
             @ApiResponse(responseCode = "200", description = "수정 성공"),
             @ApiResponse(responseCode = "404", description = "해당 동아리를 찾을 수 없음")
     })
-    @PostMapping("/{clubId}/images")
+    @PutMapping("/{clubId}/images")
     public ResponseEntity<SuccessResponseDto> updateClubImages(
             @PathVariable Long clubId,
             @RequestPart("keepImageIds") String keepImageIdsJson,
