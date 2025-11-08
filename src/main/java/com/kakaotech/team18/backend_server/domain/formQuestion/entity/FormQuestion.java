839a808d53a50806ee82dfc1340f40d58666aacf
ji-mim <194115@jnu.ac.kr>
2025-11-08 23:24:58 +0900

    chore: import문 정리 (#230)

--- src/main/java/com/kakaotech/team18/backend_server/domain/formQuestion/entity/FormQuestion.java
+++ src/main/java/com/kakaotech/team18/backend_server/domain/formQuestion/entity/FormQuestion.java
@@ -17,7 +17,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import java.time.LocalDate;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
