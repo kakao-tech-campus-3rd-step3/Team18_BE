ed59d3e8037398c1e12572725ee58995e48f5ae2
ji-mim <194115@jnu.ac.kr>
2025-11-10 14:17:31 +0900

    feat: AnswerRepository에 deleteAllByFormQuestionIds 메서드 추가 (#249)

--- src/main/java/com/kakaotech/team18/backend_server/domain/answer/repository/AnswerRepository.java
+++ src/main/java/com/kakaotech/team18/backend_server/domain/answer/repository/AnswerRepository.java
@@ -3,6 +3,7 @@ package com.kakaotech.team18.backend_server.domain.answer.repository;
 import com.kakaotech.team18.backend_server.domain.application.entity.Application;
 import com.kakaotech.team18.backend_server.domain.answer.entity.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -18,4 +19,9 @@ public interface AnswerRepository extends JpaRepository<Answer, Long> {
     List<Answer> findByApplicationWithFormQuestion(@Param("application") Application application);
 
     long deleteByApplication(Application application);
+
+
+    @Modifying
+    @Query("DELETE FROM Answer a WHERE a.formQuestion.id IN :formQuestionIds")
+    void deleteAllByFormQuestionIds(@Param("formQuestionIds") List<Long> formQuestionIds);
 }
