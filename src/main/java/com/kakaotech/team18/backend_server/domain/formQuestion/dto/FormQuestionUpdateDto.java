82eb3d2c78d29c7752ad8fb34128d7eb1c585e4f
ji-mim <194115@jnu.ac.kr>
2025-11-10 14:17:10 +0900

    feat: FormQuestionUpdateDto에 questionId 추가 (#249)

--- src/main/java/com/kakaotech/team18/backend_server/domain/formQuestion/dto/FormQuestionUpdateDto.java
+++ src/main/java/com/kakaotech/team18/backend_server/domain/formQuestion/dto/FormQuestionUpdateDto.java
@@ -13,6 +13,7 @@ import java.util.List;
 @Schema(description = "지원서 양식 개별 질문 수정 정보")
 public record FormQuestionUpdateDto(
         @Schema(description = "질문 ID", example = "1")
+        Long questionId,
         Long questionNum,
 
         @Schema(description = "질문 내용", example = "가장 자신 있는 프로그래밍 언어는 무엇인가요?")
