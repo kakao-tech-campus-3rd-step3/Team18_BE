9e5ce58a42875724ca8afb6e4839bb4a27d8cf49
jnujh <jnu21jihoon@gmail.com>
2026-01-29 20:41:34 +0900

    feat: updateMember 엔드포인트 추가

--- src/main/java/com/kakaotech/team18/backend_server/domain/clubMember/controller/ClubMemberController.java
+++ src/main/java/com/kakaotech/team18/backend_server/domain/clubMember/controller/ClubMemberController.java
@@ -2,11 +2,13 @@ package com.kakaotech.team18.backend_server.domain.clubMember.controller;
 
 import com.kakaotech.team18.backend_server.domain.clubMember.dto.ClubMemberResponseDto;
 import com.kakaotech.team18.backend_server.domain.clubMember.dto.ClubMemberSaveRequestDto;
+import com.kakaotech.team18.backend_server.domain.clubMember.dto.ClubMemberUpdateRequestDto;
 import com.kakaotech.team18.backend_server.domain.clubMember.service.ClubMemberService;
 import com.kakaotech.team18.backend_server.global.dto.SuccessResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -18,6 +20,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -82,5 +85,24 @@ public class ClubMemberController {
     ) throws IOException {
         clubMemberService.registerMembersByExcel(clubId, file);
         return ResponseEntity.ok(new SuccessResponseDto(true));
+    }
+
+    @Operation(summary = "동아리원 정보 수정", description = "동아리원의 정보를 부분 수정합니다. (직책 수정 불가)")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "수정 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 입력값"),
+            @ApiResponse(responseCode = "409", description = "중복 학번 (다른 멤버가 사용 중)"),
+            @ApiResponse(responseCode = "403", description = "권한 없음 (회장/운영진만 가능)"),
+            @ApiResponse(responseCode = "404", description = "해당 프로필을 찾을 수 없음")
+    })
+    @PreAuthorize("@customSecurityService.isClubAdminOrExecutive(#clubId)")
+    @PatchMapping("/{clubId}/members/{profileId}")
+    public ResponseEntity<ClubMemberResponseDto> updateMember(
+            @Parameter(description = "동아리 ID", required = true, example = "1") @PathVariable Long clubId,
+            @Parameter(description = "프로필 ID", required = true, example = "501") @PathVariable Long profileId,
+            @Valid @RequestBody ClubMemberUpdateRequestDto requestDto
+    ) {
+        ClubMemberResponseDto response = clubMemberService.updateMember(clubId, profileId, requestDto);
+        return ResponseEntity.ok(response);
     }
 }
