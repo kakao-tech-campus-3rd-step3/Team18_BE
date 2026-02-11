ebb00ff7b98d905cc2e78a2f33e9914b8b538387
jnujh <jnu21jihoon@gmail.com>
2026-02-11 14:14:45 +0900

    fix: Swagger Schema 수정

--- src/main/java/com/kakaotech/team18/backend_server/domain/clubMember/dto/ClubMemberRoleUpdateRequestDto.java
+++ src/main/java/com/kakaotech/team18/backend_server/domain/clubMember/dto/ClubMemberRoleUpdateRequestDto.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "동아리원 직책 변경 요청 DTO")
 public record ClubMemberRoleUpdateRequestDto(
-        @Schema(description = "변경할 직책 (CLUB_MEMBER, CLUB_EXECUTIVE)", example = "CLUB_EXECUTIVE")
+        @Schema(description = "변경할 직책 (CLUB_MEMBER, CLUB_EXECUTIVE, CLUB_ADMIN)", example = "CLUB_EXECUTIVE")
         @NotNull(message = "직책은 필수입니다.")
         Role role
 ) {
